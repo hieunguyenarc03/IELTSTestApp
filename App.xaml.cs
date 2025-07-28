3d5e4d701e2b745a2796512f65a3dc33f70a6d8a
Trung Hieu Nguyen <hieu.nguyenarc03@gmail.com>
2025-07-28 22:09:25 +0700

    Init Project

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace IELTSTestApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
